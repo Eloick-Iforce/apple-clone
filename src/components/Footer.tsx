b4c881a9ac57a820fbad93a5b6266ae666bafc4b
Eloick-Iforce <eloick.mickisz@protonmail.com>
2024-03-20 21:47:56 +0100

    Ajouter les composants Features, HowItWorks et Footer à App.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,43 @@
+import { footerLinks } from "../constants";
+
+const Footer = () => {
+  return (
+    <footer className="py-5 sm:px-10 px-5">
+      <div className="screen-max-width">
+        <div>
+          <p className="font-semibold text-gray text-xs">
+            Plus de moyen d'acheter:{" "}
+            <span className="underline text-blue">Trouver un Apple Store </span>
+            ou <span className="underline text-blue">
+              un autre vendeur
+            </span>{" "}
+            proche de vous
+          </p>
+        </div>
+
+        <div className="bg-neutral-700 my-5 h-[1px] w-full" />
+
+        <div className="flex md:flex-row flex-col md:items-center justify-between">
+          <p className="font-semibold text-gray text-xs">
+            Site web réalisé par{" "}
+            <a href="https://eloick.fr" className="underline text-blue">
+              Eloïck
+            </a>
+          </p>
+          <div className="flex">
+            {footerLinks.map((link, i) => (
+              <p key={link} className="font-semibold text-gray text-xs">
+                {link}{" "}
+                {i !== footerLinks.length - 1 && (
+                  <span className="mx-2"> | </span>
+                )}
+              </p>
+            ))}
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
